8d31e247e5c24977f277d338d5fcebeb35fe322d
Bart Kang <bart.kang.0719@gmail.com>
2025-02-04 15:39:57 -0800

    make data dynamic

---
+++ src/app/my-urls/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function URLEdit() {
+  return <div>URLEdit</div>;
+}
--- src/app/my-urls/page.tsx
+++ src/app/my-urls/page.tsx
@@ -7,36 +7,62 @@ import {
   CardHeader,
   CardTitle
 } from "@/components/ui/card";
+import { prisma } from "@/lib/prisma";
 import { FilePenLine, Trash2 } from "lucide-react";
+import Link from "next/link";
 
-export default function MyURLs() {
+export default async function MyURLs() {
+  // get the data
+  const userId = "me";
+  const urls = await prisma.url.findMany({ where: { userId } });
+  console.log(urls);
+
+  // map the data and populate them
+
   return (
     <div className="h-full">
       <h2 className="text-2xl font-extrabold mb-4">Your recent TinyURLS</h2>
 
-      <Card className="rounded-sm">
-        <CardHeader className="pb-0">
-          <CardTitle className="font-bold text-xl leading-none">
-            tinyurl.com/39wfz3xx
-          </CardTitle>
-          <CardDescription className="text-secondary">
-            https://nextjs.org/
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="pb-2 flex space-x-4 font-light text-foreground">
-          <p>1 click</p>
-          <p>|</p>
-          <p>an hour ago</p>
-        </CardContent>
-        <CardFooter className="space-x-1">
-          <Button size="icon">
-            <FilePenLine />
-          </Button>
-          <Button size="icon" variant="destructive">
-            <Trash2 />
-          </Button>
-        </CardFooter>
-      </Card>
+      <ul>
+        {urls.map(({ id, short, long, views, updateAt }) => (
+          <li key={id}>
+            <Card className="rounded-sm">
+              <CardHeader className="pb-0">
+                <CardTitle className="font-bold text-xl leading-none">
+                  {process.env.BASE_URL}/{short}
+                </CardTitle>
+                <CardDescription className="text-secondary">
+                  {long}
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="pb-2 flex space-x-4 font-light text-foreground">
+                <p>
+                  <span className="font-normal">{views}</span> click(s)
+                </p>
+                <p>|</p>
+                <p>
+                  Created At:{" "}
+                  <span className="font-normal">
+                    {updateAt.toLocaleDateString("en-us", {
+                      dateStyle: "medium"
+                    })}
+                  </span>
+                </p>
+              </CardContent>
+              <CardFooter className="space-x-1">
+                <Link href={`/my-urls/${id}`}>
+                  <Button size="icon">
+                    <FilePenLine />
+                  </Button>
+                </Link>
+                <Button size="icon" variant="destructive">
+                  <Trash2 />
+                </Button>
+              </CardFooter>
+            </Card>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
